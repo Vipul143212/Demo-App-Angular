8fe30da4446a48343937c8b8f91572c3bc0dd6d6
Vipul <vipulupadhyay563@gmail.com>
2023-08-11 18:45:55 +0530

    Add module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import  Swal  from 'sweetalert2';
 
 @Component({
@@ -10,30 +11,38 @@ import  Swal  from 'sweetalert2';
 export class AppComponent implements OnInit {
   title = 'CrudApp';
 
-  constructor(private _toastr: ToastrService) { }
+  resigerForm: FormGroup = new FormGroup({});
+
+  constructor(private _toastr: ToastrService, private _fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this._toastr.success('Hello World!');
-    this._toastr.error('Hello World!');
-    this._toastr.warning('Hello World!');
-    this._toastr.info('Hello World!');
+    // this._toastr.success('Hello World!');
+    // this._toastr.error('Hello World!');
+    // this._toastr.warning('Hello World!');
+    // this._toastr.info('Hello World!');
 
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
-      }
+    // Swal.fire({
+    //   title: 'Are you sure?',
+    //   text: "You won't be able to revert this!",
+    //   icon: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Yes, delete it!'
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    //     Swal.fire(
+    //       'Deleted!',
+    //       'Your file has been deleted.',
+    //       'success'
+    //     )
+    //   }
+    // })
+  }
+
+  setFormState(){
+    this.resigerForm = this._fb.group({
+      
     })
   }
 
